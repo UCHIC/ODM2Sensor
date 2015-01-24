5938c8d166163321e8bdb98aa4b78e9531cfcb99
Juan Caraballo <juan.caraballo17@gmail.com>
2015-01-24 14:26:44 -0700

    Removed Drupal instance from project.

--- src/sites/all/themes/gamut/page--user.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!$logged_in) {
-    drupal_goto("user/login");
-}
-
-require(path_to_theme() . '/page.tpl.php');
-
-?>
--- src/sites/all/themes/gamut/template.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-global $user;
-if ($user->uid == 0 && arg(0) != 'user'){
-  drupal_goto('user/login');
-}
