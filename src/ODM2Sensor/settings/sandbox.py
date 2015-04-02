87470c891d94dd94443781e29afee5f6d2a68d79
Mario <marioscience@hotmail.com>
2015-04-02 13:29:54 -0600

    Added configuration for sandbox server

--- src/ODM2Sensor/settings/sandbox.py
+++ src/ODM2Sensor/settings/sandbox.py
@@ -1,1 +1,14 @@
-# TODO: write configuration for sandbox
+from ODM2Sensor.settings.base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+SITE_ROOT = os.environ['APPL_PHYSICAL_PATH']
+SITE_URL = os.environ['APPL_VIRTUAL_PATH'] + '/'
+
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATIC_URL = SITE_URL + 'static/'
+
+
+import django
+django.setup()
