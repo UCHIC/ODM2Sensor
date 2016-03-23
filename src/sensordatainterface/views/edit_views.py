314f39c88b2d39d0a9e6441cfe999c98e0cb6ae2
ZachYoshikawa <zach.yoshikawa@gmail.com>
2016-03-23 15:36:15 -0600

    Removed redundant code. Still working on fixing equipment_used list

--- src/sensordatainterface/views/edit_views.py
+++ src/sensordatainterface/views/edit_views.py
@@ -1162,8 +1162,15 @@ def edit_action(request, action_type, action_id=None, visit_id=None, site_id=Non
         )
         site_visit = Action.objects.get(pk=parent_action_id.relatedactionid.actionid)
         site_visit_form = SiteVisitChoiceForm(instance=site_visit)
-        equipment_used = EquipmentUsed.objects.filter(actionid=child_action)
 
+
+        # @todo issues #171, #189 equipment_used list is not populating
+        equipment_used = EquipmentUsed.objects.filter(actionid=child_action)
+        current_equipment = [equ.equipmentid.equipmentid for equ in EquipmentUsed.objects.filter(actionid=child_action)]
+
+        # place
+        for balls in equipment_used:
+            print balls.equipmentid.equipmentid
         action_form = ActionForm(
             instance=child_action,
             initial={'equipmentused': [equ.equipmentid.equipmentid for equ in equipment_used]}
@@ -1177,9 +1184,6 @@ def edit_action(request, action_type, action_id=None, visit_id=None, site_id=Non
             action_form.initial['calibrationequation'] = CalibrationAction.objects.get(pk=action_id).calibrationequation
 
         elif action_type == 'EquipmentRetrieval' or action_type == 'InstrumentRetrieval':
-            action_form = ActionForm(
-                initial={'equipmentused': [equ.equipmentid.equipmentid for equ in equipment_used]}
-            )
             action_form.initial['actionid'] = None
             action_form.initial['methodid'] = None
             action_form.initial['actiontypecv'] = CvActiontype.objects.get(term='equipmentRetrieval' if child_action.actiontypecv.term == 'equipmentDeployment' else 'instrumentRetrieval')
